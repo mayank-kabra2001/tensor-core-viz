463f8dd8c526f095844b2052a9625eb84cc3be0c
Mayank Kabra <57330493+mayank-kabra2001@users.noreply.github.com>
2021-06-29 20:49:19 +0530

    Update tc_sram.sv

--- tc_sram.sv
+++ tc_sram.sv
@@ -83,7 +83,7 @@ module tc_sram #(
   addr_t [NumPorts-1:0] r_addr_q;
 
   // SRAM simulation initialization
-  data_t [NumWords-1:0] init_val;
+  /*data_t [NumWords-1:0] init_val;
   initial begin : proc_sram_init
     for (int unsigned i = 0; i < NumWords; i++) begin
       for (int unsigned j = 0; j < DataWidth; j++) begin
@@ -95,7 +95,7 @@ module tc_sram #(
         endcase
       end
     end
-  end
+  end*/
 
   // set the read output if requested
   // The read data at the highest array index is set combinational.
